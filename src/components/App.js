07cf29c2322106ca3410abe11f6357cc5376dd94
tomkjose <tomjosekunnumpurath@gmail.com>
2023-07-31 21:34:25 +0530

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
- import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+ import { HashRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "../pages/Home";
 import MealPage from "../pages/MealPage";
 import CategoryPage from "../pages/CategoryPage";
